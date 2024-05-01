7c635a0f6f8928b4c51d04eee05bcda52f2fb5ce
CarlosE28 <163207353+CarlosE28@users.noreply.github.com>
2024-04-30 23:09:28 -0300

    Tela de login,1 entrega

---
+++ NetBeansProjects/ProjetoaLogin/src/main/java/com/mycompany/projetoalogin/ProjetoaLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoalogin;
+
+/**
+ *
+ * @author marav
+ */
+public class ProjetoaLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
